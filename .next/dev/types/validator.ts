b7c8a9d279456dc5377254e816d6ab3c4e2cb276
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2026-01-07 16:36:04 -0500

    added video call functionality pages with status card component

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -45,6 +45,15 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
+// Validate ../../../app/(signed-in)/dashboard/video-call/[id]/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/dashboard/video-call/[id]">> = Specific
+  const handler = {} as typeof import("../../../app/(signed-in)/dashboard/video-call/[id]/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../../app/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/">> = Specific
@@ -59,6 +68,15 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
 
 
 
+
+// Validate ../../../app/(signed-in)/dashboard/video-call/[id]/layout.tsx
+{
+  type __IsExpected<Specific extends LayoutConfig<"/dashboard/video-call/[id]">> = Specific
+  const handler = {} as typeof import("../../../app/(signed-in)/dashboard/video-call/[id]/layout.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
 
 // Validate ../../../app/(signed-in)/layout.tsx
 {
